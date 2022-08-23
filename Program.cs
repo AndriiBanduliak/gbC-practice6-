d87c2a3ee5bb689f7a09a78e71af37a82f13b6bd
Andrii Banduliak <banduliak1303@gmail.com>
2022-08-23 20:01:17 +0300

    1st task is done

--- Program.cs
+++ Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+//0, 7, 8, -2, -2-> 2
+
+//1, -7, 567, 89, 223-> 3
+
+using static System.Console;
+
+WriteLine("How many numbers do you want to enter ?");
+int length=int.Parse(ReadLine());
+int[]digits=getArr(length);
+printArr(digits);
+printCount(digits);
+
+
+void printCount(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0) count++;
+    }
+    Write($" ->{count}");
+}
+void printArr(int[]arr)
+{
+    for (int i=0; i<digits.Length; i++)
+    {
+        Write(digits[i]);
+        if (i < digits.Length - 1) Write(", ");
+    }
+    
+}
+int[] getArr(int length)
+{
+    int[] arr=new int[length];
+    for (int i=0; i<length; i++)
+    {
+        WriteLine($"Number  {i+1}");
+        arr[i] = int.Parse(ReadLine());
+    }
+    return arr;
+}
